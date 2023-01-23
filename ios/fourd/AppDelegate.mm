345d24dcf94d9c6564e3b5e31026849ff5abed0a
Kyle Gorges <kyle.gorges@kochind.com>
2023-01-23 15:05:43 -0600

    chore: upgrade to 71.1

--- ios/fourd/AppDelegate.mm
+++ ios/fourd/AppDelegate.mm
@@ -18,6 +18,11 @@
   }
 
   self.moduleName = @"fourd";
+
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
